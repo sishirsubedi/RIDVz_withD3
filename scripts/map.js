6c900f52a2bc87f295bb7e0710db0f74c419a617
sishirsubedi <subedisishir@gmail.com>
2018-03-18 20:24:50 -0500

    added welcome animation

--- scripts/map.js
+++ scripts/map.js
@@ -6,18 +6,18 @@ var zoom = d3.behavior.zoom()
     .on("zoom", move);
 
 
-var width = document.getElementById('container').offsetWidth;
+var width = document.getElementById('div_wmap').offsetWidth;
 var height = width / 2;
 
 var topo,projection,path,svg,g;
 
 var graticule = d3.geo.graticule();
 
-var tooltip = d3.select("#container")
+var tooltip = d3.select("#div_wmap")
 				.append("div")
 				.attr("class", "tooltip hidden")
 
-                
+
 setup(width,height);
 
 function setup(width,height){
@@ -27,7 +27,7 @@ function setup(width,height){
 
   path = d3.geo.path().projection(projection);
 
-  svg = d3.select("#container").append("svg")
+  svg = d3.select("#div_wmap").append("svg")
       .attr("width", width)
       .attr("height", height)
       .call(zoom)
@@ -72,8 +72,8 @@ function draw(topo) {
       //.style("fill",'blue');
 
   //offsets for tooltips
-  var offsetL = document.getElementById('container').offsetLeft+20;
-  var offsetT = document.getElementById('container').offsetTop+10;
+  var offsetL = document.getElementById('div_wmap').offsetLeft+20;
+  var offsetT = document.getElementById('div_wmap').offsetTop+10;
 
   //tooltips
   country.on("mousemove", function(d,i) {
@@ -83,13 +83,13 @@ function draw(topo) {
       console.log(d.properties.name)
 
       tooltip.classed("hidden", false)
-                .html("thumbnailURL")
+                .html("<h1>Bar Graph</h1><br> <svg class='chart'></svg>")
                 .style("left", (d3.event.pageX - 34) + "px")
                 .style("top", (d3.event.pageY - 12) + "px")
 
      // .style("fill", "blue");
      
-      
+ 
      
       })
 
@@ -112,7 +112,7 @@ function draw(topo) {
 
 
 function redraw() {
-  width = document.getElementById('container').offsetWidth;
+  width = document.getElementById('div_wmap').offsetWidth;
   height = width / 2;
   d3.select('svg').remove();
   setup(width,height);
@@ -157,7 +157,7 @@ function throttle() {
 }
 
 
-//geo translation on mouse click in map
+//geo translation on mouse click in div_wmap
 function click() {
   var latlon = projection.invert(d3.mouse(this));
   console.log(latlon);
@@ -166,7 +166,7 @@ function click() {
 }
 
 
-// //function to add points and text to the map (used in plotting capitals)
+// //function to add points and text to the div_wmap (used in plotting capitals)
 // function addpoint(lat,lon,text) {
 
 //   var gpoint = g.append("g").attr("class", "gpoint");
@@ -191,3 +191,124 @@ function click() {
 
 // }
 
+// make bar graph
+var width = 300,
+    height = 300;
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var chart = d3.select(".chart")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.tsv("data.tsv", type, function(error, data) {
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  var barWidth = width / data.length;
+
+  var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+
+  bar.append("rect")
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("width", barWidth - 1);
+
+  bar.append("text")
+      .attr("x", barWidth / 2)
+      .attr("y", function(d) { return y(d.value) + 3; })
+      .attr("dy", ".75em")
+      .text(function(d) { return d.value; });
+});
+
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
+}
+
+
+// var data;
+//     var scale = 1.25
+//     var delay = 1600
+//     var canvas = d3.select(".chart")
+//         .attr("width", width)
+//         .attr("height", height);
+//         .style("border", "1px solid #111")
+
+//     var line =  d3.line()
+//         .x(function(d) { return d.x })
+//         .y(function(d) { return d.y })
+//         .curve(d3.curveBasis)
+
+//     d3.json("data/house_rep.json", function(err, faces) {      
+//         render(0)
+//         function render(i) {
+//             var drawing = faces[i]
+//             var strokes = strokifyDrawing(drawing.drawing)
+//             center(strokes)
+
+//             var ps = canvas.selectAll("path").data(strokes)
+//             ps.exit().remove()
+//             var psE = ps.enter().append("path")
+//             ps = psE.merge(ps)
+//             ps
+//                 .attr("d", line)
+//                 .style("fill", "none")
+//                 .style("stroke", "#111")
+//                 .style("stroke-width", 3)
+//                 .style("stroke-linecap", "round")
+        
+//         } 
+//   })
+//     function center(strokes) {
+//         var minY = Infinity
+//         var maxY = -Infinity
+//         var minX = Infinity
+//         var maxX = -Infinity
+//         var centroidX = 0
+//         var centroidY = 0
+//         var count = 0
+//         strokes.forEach(function(stroke) {
+//             stroke.forEach(function(p) {
+//             centroidX += p.x
+//             centroidY += p.y
+//             count++
+//             })
+//         })
+//         centroidX /= count;
+//         centroidY /= count;
+//         strokes.forEach(function(stroke) {
+//             stroke.forEach(function(p) {
+//                 p.x *= scale
+//                 p.y *= scale
+//                 p.x += width/2 - centroidX * scale
+//                 p.y += height/2 - centroidY * scale
+//                 if(p.y < minY) minY = p.y
+//                 if(p.y > maxY) maxY = p.y
+//                 if(p.x < minX) minX = p.x
+//                 if(p.x > maxX) maxX = p.x
+//             })
+//         })
+//         var diffX = minX - (width - maxX)
+//         var diffY = minY - (height - maxY)
+//         strokes.forEach(function(stroke) {
+//             stroke.forEach(function(p) {
+//                 p.x -= diffX/2
+//                 p.y -= diffY/2
+//             })
+//         })
+//     }
+//     function strokifyDrawing(drawing) {
+//         var strokes = drawing.div_wmap(function(s) {
+//             var points = []
+//             s[0].forEach(function(x,i) {
+//                 points.push({x: x, y: s[1][i] })
+//             })
+//             return points;
+//         })
+//         return strokes
+//     }
+// }
